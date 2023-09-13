989710709403fa0d4db2d7ce79ff9e8b1ea1dba2
jsochoaco <sebastianochoa05@gmail.com>
2023-09-13 13:52:36 -0500

    Actions Admin Profile

---
+++ client/src/paginas/LoginAdmin/LoginAdmin.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+
+const LoginAdmin = () => {
+    const [existe, setExiste] = useState(null)
+    
+    const userAdmin = (userData) => {
+        try {
+            const {email} = userData;
+            const URL = "";
+            const query =  `?email=${email}`;
+            const endpoint = URL + query;
+            return async () => {
+                const {data} = await axios.get(endpoint);
+                const {existe} = data;
+                setExiste(existe)
+            }
+        }
+        catch (error) {
+            const errorResponse = {}
+            errorResponse.error = error.message
+            return errorResponse
+        }
+    }
+    // Renderizado
+    return (
+        <div>
+            <div>
+                <img src="" alt=""/>
+                <form onSubmit={handleSummit}>
+                    <input type="text" name="email" value={email}/>
+                    <button onClick={()=> userAdmin(email)}>Siguiente</button>
+
+                </form>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+};
+export default LoginAdmin
