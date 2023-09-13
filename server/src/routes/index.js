897fc58a275a7585fb8caf802a2f78df519406e2
Sandra Piza <131202421+sandra7777-P@users.noreply.github.com>
2023-09-13 16:25:05 -0500

    paginacion desde el back

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const usersRouters = require("./usersRouters");
 const productsRouters = require("./productsRouters.js");
 const categoryRouter = require("./categoryRouters.js");
+const paginationRouters = require("./paginationRouters");
 const router = Router(); // Creamos una instancia de Router
 
 // Creamos la redirección al router correspondiente
 router.use("/users", usersRouters);
 router.use("/products", productsRouters);
 router.use("/categories", categoryRouter);
+router.use("/pagination", paginationRouters);
 
 module.exports = router; // Exportamos el router configurado
