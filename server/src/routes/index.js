24383d86767b8cad99027bd01278b444a4357a5e
JosePenya <josemariapep9@gmail.com>
2023-09-13 17:13:08 -0500

    Cambio de nombre en model

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,10 +2,13 @@ const { Router } = require("express");
 const usersRouters = require("./usersRouters");
 const productsRouters = require("./productsRouters.js");
 const categoryRouter = require("./categoryRouters.js");
+const loginRouters = require('./loginRouters')
+
 const router = Router(); // Creamos una instancia de Router
 
 // Creamos la redirección al router correspondiente
 router.use("/users", usersRouters);
+router.use("/auth", loginRouters);
 router.use("/products", productsRouters);
 router.use("/categories", categoryRouter);
 
