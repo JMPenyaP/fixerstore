ddc686793ec3504c6611cb19573daf269de6945d
marcos galliano <marquitosgalliano@hotmail.com>
2023-09-14 15:55:15 -0300

    agregando server en detail component

--- client/src/paginas/DetailPage/DetailPage.jsx
+++ client/src/paginas/DetailPage/DetailPage.jsx
@@ -1,44 +1,39 @@
 import style from "./DetailPage.module.css";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const DetailPage = () => {
   const [loading, setLoading] = useState(true);
-  const [product, setProduct] = useState({
-    name: "Zapatilla Nike Air Max",
-    description:
-      "Las Zapatillas Nike Air Max son la elección perfecta para los entusiastas del running. Diseñadas con tecnología de vanguardia y materiales de alta calidad, estas zapatillas ofrecen comodidad, durabilidad y rendimiento inigualables. Con su amortiguación avanzada y su suela resistente, te brindan un soporte excepcional para tus pies en cada paso. Ya sea que estés corriendo en el parque o entrenando para tu próxima carrera, las Zapatillas Nike Air Max te llevarán al siguiente nivel.",
-    image:
-      "https://http2.mlstatic.com/D_NQ_NP_632124-MLA42150136966_062020-O.webp",
-    price: 99.99,
-    color: "Negro",
-    talla: "42",
-    marca: "Nike",
-    categoria: "Deportes",
-    enOferta: true,
-    stock: 50,
-  });
-  const [mainImage, setMainImage] = useState(product.image);
+  const [product, setProduct] = useState({});
+  const [mainImage, setMainImage] = useState();
   const params = useParams();
   const id = params.id;
 
   useEffect(() => {
     try {
-      setLoading(false);
+      axios(`http://localhost:3001/products/${id}`).then(({ data }) => {
+        if (data.name) {
+          setProduct(data);
+          console.log(data);
+          setMainImage(data.firstImage)
+        }
+        setLoading(false);
+      });
     } catch (error) {
       console.log(error);
     }
   }, [id]);
+
+  const setImage = (url) => {
+    setMainImage(url);
+  };
 
   const images = [
     "https://http2.mlstatic.com/D_NQ_NP_632124-MLA42150136966_062020-O.webp",
     "https://http2.mlstatic.com/D_NQ_NP_817118-MLA42150173428_062020-O.webp",
     "https://http2.mlstatic.com/D_NQ_NP_816748-MLA42150191129_062020-O.webp",
   ];
-
-  const setImage = (url) => {
-    setMainImage(url);
-  };
 
   return (
     <div className={style.divDetail}>
@@ -52,13 +47,23 @@ const DetailPage = () => {
             </div>
             <div className={style.carouselImages}>
               {images.map((image, index) => {
-                return <div className={style.divImageCarousel} key={index}><img src={image} alt="product" onClick={() => {setImage(image)}}/></div>;
+                return (
+                  <div className={style.divImageCarousel} key={index}>
+                    <img
+                      src={image}
+                      alt="product"
+                      onClick={() => {
+                        setImage(image);
+                      }}
+                    />
+                  </div>
+                );
               })}
             </div>
           </div>
           <div className={style.infoDiv}>
             <h1>{product.name}</h1>
-            <h2>${product.price}</h2>
+            <h2>${product.priceOfList}</h2>
             <h3>{product.description}</h3>
             <div className={style.divButton}>
               <button>Agregar Al Carrito</button>
