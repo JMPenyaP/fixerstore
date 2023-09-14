be54c5b01009a7760ebdad6b08fbf318cec12f49
JosePenya <josemariapep9@gmail.com>
2023-09-14 13:11:30 -0500

    Update User Login

--- server/src/routes/usersRouters.js
+++ server/src/routes/usersRouters.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const usersRouters = Router();
-const { getAllUsersHandler } = require("../handlers/usersHandlers");
+const { getAllUsersHandler, getUserEmailHandler } = require("../handlers/usersHandlers");
 const { createUserHandler } = require('../controllers/usersControllers');
 
 usersRouters.post("/register", createUserHandler);
 usersRouters.get("/", getAllUsersHandler);
+usersRouters.get("/email", getUserEmailHandler);
 
 module.exports = usersRouters;
