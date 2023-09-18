50af5ce00845b14cda3757181c639b113df77606
AnsonyRoja <ansonyrojas2015@gmail.com>
2023-09-18 19:31:33 -0400

    update change to state false = true

--- server/src/hardcoded/products.js
+++ server/src/hardcoded/products.js
@@ -10,9 +10,9 @@ const products = [
     description: "producto para zapatos",
     date: "01-18-2023",
     priceOfList: 95.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
 
   },
@@ -25,9 +25,9 @@ const products = [
     description: "CORDONES REFLECTIVOS, DECORAN MUY BONITO EL CALZADO, TIENE MUY BUENA RESITENCIA Y REFLEJAN EN LA OSCURIDAD, SE UTILIZAN EN LAS BOTAS Y LOS TENIS.",
     date: "01-18-2023",
     priceOfList: 5,
-    statusOffer: false,
+    statusOffer: true,
     offer: 25.000,
-    status: false,
+    status: true,
     stock: 15
 
   },
@@ -40,9 +40,9 @@ const products = [
     description: "SON HORMAS EN MATERIAL DE PLASTICO, PERO NO TE DEJES ENGAÑAR SU COMPLICADA ESTRUCTURA TIENE LA FUERZA Y DURABILIDAD SUFICIENTE PARA MANTER LA FORMA DE TU ZAPATOS, E IMPIDEN QUE COMIENCEN A SALIR ARRUGAS POR LOS DOBLECES QUE SE REALIZAN A LA ACCION DE CAMINAR O TROTAR, SON RECOMENDABLES EN ZAPATOS DE HORMA MEDIANA ( BOTINES, TACONES Y TENIS DE DAMA).",
     date: "01-18-2023",
     priceOfList: 35.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
 
   },
@@ -55,9 +55,9 @@ const products = [
     description: "SON PLANTILLAS TRANSPIRABLES, DE MATERIAL EVA TE AYUDAN A LA COMODIDAD DE LA PARTE DEL TALON Y A UNA MICRO TRANSPIRACION DE TUS PIES.SIRVEN PARA HACER DEPORTE E INCLUSO CAMINAR, SE UILIZAN MUCHO EN TENIS DEPORTIVOS.",
     date: "01-18-2023",
     priceOfList: 65.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
 
   },
@@ -70,9 +70,9 @@ const products = [
     description: "ESTE TIPO DE TALONERA TIENE EN UN COSTADO MAS LEVATAMIENTO, POR LO QUE TE AYUDA A DARLE UNIFORMIDAD A TU CAMINAR EN CASO DE QUE APOYES MAS EN UN COSTADO O TENGAS UN PROBLEMA ANATOMICO EN LA PARTE DEL TALON. POR SU MATERIAL EN GEL ES ERGONOMICA Y TE AYUDA AL AMORTIGUO DEL TALON. SE UTILIZA EN TENI, BOTA Y ZAPATO FORMAL.",
     date: "01-18-2023",
     priceOfList: 45.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
 
   },
@@ -85,9 +85,9 @@ const products = [
     description: "ES UN CEPILLO DE LIMPIEZA PROFESIONAL PARA LA GAMUZA, TIENE UN LADO DE CERDAS DE ALAMBRE QUE NOS AYUDA A PEINAR Y EMPAREJAR LA GAMUZA, TAMBIEN AYUDA AL PROCESO DE LA TINTURA PARA QUE EL MATERIAL ABRA EL PORO Y PENETRE MEJOR EL QUIMICO, AL COSTADO TIENE UNOS DIENTES DE GOMA QUE AYUDA A LIMPIAR EL MUGRE O POLVO POR LOS ORIFICIOS DE TU CALZADO ENTRE LA CAPELLA Y SUELA, Y AL RESPALDO HAY CERDAS DE GOMA QUE TE AYUDAN A LIMPIAR LAS MOTAS Y SUCIEDAD QUE ESTE ALMACENE.",
     date: "01-18-2023",
     priceOfList: 25.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
 
   },
@@ -100,9 +100,9 @@ const products = [
     description: "ESTE EN UN QUIMICO QUE VIENE EN SU PRESENTACION DE SPRAY O ATOMIZADOR, AYUDA A RENOVAR EL TONO DEL CALZADO SOLO EN CASO DE QUE ESTE SE ENCUENTRE UN POCO DETERIORADO, POR LO QUE ES NEUTRO NO DA TONALIDAD, NO TAPA RAPONES GRANDES, NO EMPAREJA UN ZAPATO DESCOLORIDO, ES RECOMENDABLE UTILIZARLO EN NOBUCK, GAMUZA , CARNZA Y TEXTIL.",
     date: "01-18-2023",
     priceOfList: 20.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
 
   },
@@ -115,9 +115,9 @@ const products = [
     description: "LOS SHIELD, SON SOPORTES INTERNOS, QUE NOS AYUDAN A QUE EL CALZADO NO PIERDA SU FORMA EN LA PARTE DEL EMPEINE, SON MATERIAL PLASTICO MUY MALEABLE. EIMPIDEN DE QUE ELLOS SE ARRUGEN EN EL DOBLES DEL CAMINAR (EMPEINE)",
     date: "01-18-2023",
     priceOfList: 35.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
 
   },
@@ -130,9 +130,9 @@ const products = [
     description: "ESTE CALZADOR ES DE PASTA , ETE AYUDA A PONERTE LOS ZAPATOS CON MAS FACILILDAD EN LA PARTE DEL TALON. TOCA INCLINARSE POR LO QUE ES CORTO.",
     date: "01-18-2023",
     priceOfList: 15.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
 
 
@@ -147,9 +147,9 @@ const products = [
     description: "SON PLANTILLAS, AYUDAN PARA LA PARTE DE LA PLANTA DEL PIE EN LA ZONA INFEERIOR (LOS METATARSOS) SE UTILIZAN EN LA MAYORIA DE CALZADO PREFERIBLEMENTE EN TACONES O ZAPATOS QUE TENGAN TACON. TE DAN COMODIDAD Y SON EN MATERIAL SILICONA",
     date: "01-18-2023",
     priceOfList: 25.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
 
   },
@@ -161,9 +161,9 @@ const products = [
     description: "SON PLANTILLAS, AYUDAN PARA LA PARTE DE LA PLANTA DEL PIE EN LA ZONA INFEERIOR (LOS METATARSOS) SE UTILIZAN EN LA MAYORIA DE CALZADO PREFERIBLEMENTE EN TACONES O ZAPATOS QUE TENGAN TACON. TE DAN COMODIDAD Y SON EN MATERIAL SILICONA",
     date: "01-18-2023",
     priceOfList: 25.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
   },
   {
@@ -175,9 +175,9 @@ const products = [
     description: "ESTE EN UN QUIMICO QUE VIENE EN SU PRESENTACION DE SPRAY O ATOMIZADOR, AYUDA A RENOVAR EL TONO DEL CALZADO SOLO EN CASO DE QUE ESTE SE ENCUENTRE UN POCO DETERIORADO, POR LO QUE ES NEUTRO NO DA TONALIDAD, NO TAPA RAPONES GRANDES,  NO EMPAREJA UN ZAPATO DESCOLORIDO, (ES SIMILAR AL LIMPIADOR EXPRESS), ES RECOMENDABLE UTILIZARLO EN NOBUCK, GAMUZA , CARNZA  Y TEXTIL.",
     date: "01-18-2023",
     priceOfList: 25.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
 
   },
@@ -189,9 +189,9 @@ const products = [
     description: "ESTE CREMA AYUDA A HIDRATAR Y REJUVENCER  EL CUERO, SU TONALIDAD ES NEUTRA (SIRVE PARA TODOS LOS COLORES),  TAPAS RASPONES SOLO EN CASO DE QUE SU CALZADO NO SE ENCUENTRE DESCOLORIDO. ES RECOMENDABLE SOLO PARA EL CUERO QUE TENGA SU CONTEXTURA LISA.",
     date: "01-18-2023",
     priceOfList: 25.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
 
   },
@@ -204,9 +204,9 @@ const products = [
     description: "ES UN KID DE LIMPIEZA PROFESIONAL, INCLUYE: SHAMPOO ESPECIALIZADO PARA UNA LIMPIEZA PROFUNDA DE TONALIDAD NEUTRA, INCLUYE TAMBIEN UN CEPILLO QUE TIENE LAS CERDAS SUAVES PARA MAYOR PENETRACION EN LOS POROS Y MEJOR TRATO DEL MATERIAL, PARA HACER EL POROCESO DE REFREGAR Y SACARLE TODA LA SUCIEDAD QUE ESTE CONTEGA,  Y POR ULTIMO INCLUIMOS EL TRAPITO DE MICROFIBRA QUE AYUDA A RETIRAR LA ESPUMA SE VA FORMANDO POR EL PROCESO DE REFREGADO. TODO ESTO ESTA EMPACADO EN SU CAJITA CORRESPONDIENTE PARA SU PRESENTACION.",
     date: "01-18-2023",
     priceOfList: 55.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
 
   },
@@ -220,9 +220,9 @@ const products = [
     description: "ESTE LIMPIADOR EN SECO VIENE EN UN FRASCO DE SPRAY O ATOMIZADOR, AYUDA A LA LIMPIEZA DEL CALZADO, ES RECOMENDABLE UTILIZARLO EN GAMUZA, ANTE, CARNAZA, NOBUCK Y EN ALGUNOS TEXTILES, NO SE REQUIERE DE UNA LIMPIEZA ESPECIALIDA, NI DE NINGUN OTRO QUIMICO",
     date: "01-18-2023",
     priceOfList: 35.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
 
   },
@@ -236,9 +236,9 @@ const products = [
     description: "SON HORMAS EN MATERIAL DE MADERA DE CEDRO, MUY BUEN MATERIAL Y RESITENTE PARA  MANTENER LA FORMA DE TUS ZAPATOS, ADEMAS AYUDAN A ANCHARLOS Y ALARGARLOS POR UNOS MILIMETROS E IMPIDEN QUE SE TE ARRUGEN TUS ZAPATOS. SE UTILIZAN EN HORMAS DE ZAPATOS GRANDES (ES MAS FRECUNTE EN CALZADO  DE CABALLEROS), VERBIGRACIA: BOTAS, ZAPATOS FORMALES",
     date: "01-18-2023",
     priceOfList: 85.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
 
 
@@ -253,9 +253,9 @@ const products = [
     description: "Son plantillas que ayudan a subir estatura, hechas para el cuidado del talon de la parte interna del zapato",
     date: "01-18-2023",
     priceOfList: 45.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
 
 
@@ -269,9 +269,9 @@ const products = [
     description: "ESTOS CORDONES , LE DAN UN ESTILO MUY BONITO A TU CALZADO, ADEMAS RESISTENTES Y DURABLES, SE UTILIZAN FRECUENTEMENTE EN LAS BOTAS Y ZAPATOS EN CUERO",
     date: "01-18-2023",
     priceOfList: 15.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
 
   },
@@ -284,9 +284,9 @@ const products = [
     description: "ES UN CALZADOR PLASTICO DOBLE, POR EL LARGOR DEL MISMO AYUDA A PONERTE LOS ZAPATOS CON MAS FACILIDAD EN LA PARTE DEL TALON, NO TIENES QUE INCLINARTE POR SU TAMAÑO EN LUGAR DE OTROS QUE SON MAS CORTOS.",
     date: "01-18-2023",
     priceOfList: 25.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
 
 
@@ -299,9 +299,9 @@ const products = [
     description: "ESTAS PLANTILLAS SON EN MATERIAL EN GEL, NOS AYUDAN A LA COMODIDAD DE LA PLANTA DEL PIE, TIENE UNA SUPERFICIE QUE SOBRESALE EN SU MITAD QUE SIRVE PARA EL PIE PLANO Y SU COMODIDAD. NO AYUDA A TRATAR EL CASO, NO OBSTANTE; LE DA COMODIDAD Y ESTABILIDAD A LA HORA DE CAMINAR, POR SU MATERIAL HERGONOMICO Y FLEXIBLE, AYUDA AL AMORIGUO Y DESCANSO DEL PIE",
     date: "01-18-2023",
     priceOfList: 35.000,
-    statusOffer: false,
+    statusOffer: true,
     offer: 10,
-    status: false,
+    status: true,
     stock: 15
 
   }
