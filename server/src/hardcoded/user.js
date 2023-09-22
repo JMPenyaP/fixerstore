f22bc2c50ec2ae71f94ecb2b0a99987c7309ea20
JosePenya <josemariapep9@gmail.com>
2023-09-22 10:46:54 -0500

    Actualizacion de hardcode

--- server/src/hardcoded/user.js
+++ server/src/hardcoded/user.js
@@ -1,6 +1,6 @@
 const { createUserHandler } = require('../controllers/usersControllers');
-const {User}=require("../db")
-const user = [{
+const { User } = require("../db")
+const user = {
     email: "admin@gmail.com",
     password: "123456",
     role: "admin",
@@ -10,7 +10,8 @@ const user = [{
     address: "calle 30 Kra 15 #8",
     city: "Kenedy",
     department: "Cundinamarca"
-}, {
+}
+/*{
     email: "usuario@gmail.com",
     password: "abcdef",
     role: "client",
@@ -20,7 +21,7 @@ const user = [{
     address: "calle 30 Kra 15 #8",
     city: "Kenedy",
     department: "Cundinamarca"
-}];
+};*/
 
 // const mockRequest = {
 //     body: user // Simula un cuerpo de solicitud con el objeto de usuario
@@ -36,21 +37,29 @@ const user = [{
 //         console.log(data);
 //     }
 // };
-
+/*
 async function createAdmin() {
 
-    user.forEach(async(us)=>{
+    user.forEach(async (us) => {
+        try {
+            await User.create(us);
+            console.log("Usuario administrador creado con éxito.");
+        } catch (error) {
+            console.error(error);
+            console.log("Error al crear el usuario administrador.");
+        }
+    })
+
+}*/
+async function createAdmin() {
     try {
-        await User.create(us);
+        await User.create(user);
         console.log("Usuario administrador creado con éxito.");
     } catch (error) {
         console.error(error);
         console.log("Error al crear el usuario administrador.");
     }
-    })
-    
 }
-
 
 module.exports = {
     createAdmin
