a9ea0725f066f654bf61323dc86f1fc0734859d2
AnsonyRoja <ansonyrojas2015@gmail.com>
2023-09-22 11:16:26 -0400

    actualizar repo

--- server/src/models/cartModels.js
+++ server/src/models/cartModels.js
@@ -1,12 +1,19 @@
 const { DataTypes } = require('sequelize');
 
+
+
 module.exports = (sequelize) => {
-  sequelize.define("Cart", {
 
-    quantity: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
+    sequelize.define('Cart', {
 
-  });
-};
+        quantity: {
+
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultVlaue: 1
+        }
+
+    })
+
+
+} 
