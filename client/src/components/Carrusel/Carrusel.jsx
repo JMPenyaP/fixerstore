6be87eccfea3538aa1e5150ef7aac608d793f8fa
marcos galliano <marquitosgalliano@hotmail.com>
2023-09-26 22:02:17 -0300

    responsive nav

--- client/src/components/Carrusel/Carrusel.jsx
+++ client/src/components/Carrusel/Carrusel.jsx
@@ -1,4 +1,4 @@
-import style from "./Carrusel.css";
+import style from "./carrusel.css";
 import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
 import axios from "axios";
