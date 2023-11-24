f320959ab6abc0551c0b6e25f85548afc1798c47
CHEMA <chema.penya@gmail.com>
2023-11-24 07:15:14 -0500

    git test

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
